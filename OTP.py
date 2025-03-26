522df8bab1a80fbb3f5721a7cde351707aa6c5b9
Dasari Purushotham Deepak <purushottamd215@gmail.com>
2025-03-26 13:29:49 +0530

    Add files via upload

---
+++ OTP.py
@@ -0,0 +1,36 @@
+import smtplib
+import random
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+otp = random.randint(1111,9999)
+body = f"OTP for Verification is {otp}"
+
+msg = MIMEMultipart()
+msg["From"] = "purushottamd215@gmail.com"
+msg["To"] = input("Enter Email Id: ")
+msg["Subject"] = "OTP For Validation"
+msg.attach(MIMEText(body,'html'))
+
+server = smtplib.SMTP("smtp.gmail.com",587)
+server.starttls()
+server.login("purushottamd215@gmail.com","budl blxa cdgu helh")
+server.send_message(msg)
+server.quit()
+
+cotp = int(input("Enter OTP Received: "))
+if otp == cotp:
+    print("OTP verification Success")
+else:
+    print("Invalid OTP")
+    
+
+
+
+
+
+
+
+
+
+
